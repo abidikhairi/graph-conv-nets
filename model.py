b52744bdce7b02e831d1965bf433987343f023a2
abidikhairi <kh41r1.abidi@gmail.com>
2022-01-19 00:18:43 +0100

    train gcn on cora dataset

---
+++ model.py
@@ -0,0 +1,20 @@
+from torch import nn
+from dgl.nn.pytorch import GraphConv
+
+
+class GCN(nn.Module):
+	def __init__(self, feature_size: int, num_classes: int):
+		super().__init__()
+		
+		self.conv1 = GraphConv(in_feats=feature_size, out_feats=16, activation=nn.ReLU())
+		self.dropout = nn.Dropout(p=0.5)
+		self.conv2 = GraphConv(in_feats=16, out_feats=num_classes)
+		self.softmax = nn.LogSoftmax(dim=1)
+
+	def forward(self, graph, n_feats):
+		
+		x = self.conv1(graph, n_feats)
+		x = self.dropout(x)
+		x = self.conv2(graph, x)
+
+		return self.softmax(x)
---
+++ train.py
@@ -0,0 +1,43 @@
+import torch
+import torchmetrics.functional as metrics
+from dgl.data import CoraGraphDataset
+from model import GCN
+
+if __name__ == '__main__':
+	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+	epochs = 100
+	dataset = CoraGraphDataset()
+	
+	graph = dataset[0]
+
+	train_idx = torch.nonzero(graph.ndata['train_mask'], as_tuple=False).flatten()
+	test_idx = torch.nonzero(graph.ndata['test_mask'], as_tuple=False).flatten()
+
+	model = GCN(feature_size=1433, num_classes=7)
+	criterion = torch.nn.NLLLoss()
+	optimizer = torch.optim.Adam(model.parameters(), lr=0.1, weight_decay=5e-4)
+
+	model.to(device)
+	graph.to(device)
+
+	for epoch in range(epochs):
+		model.train()
+		optimizer.zero_grad()
+		n_feats = graph.ndata['feat']
+		labels = graph.ndata['label']
+
+		logits = model(graph, n_feats)
+
+		loss = criterion(logits[train_idx], labels[train_idx])
+
+		loss.backward()
+		optimizer.step()
+
+		with torch.no_grad():
+			model.eval()
+			output = model(graph, n_feats)
+
+			loss = criterion(output, labels)
+			accuracy = metrics.accuracy(output, labels)
+
+			print(f'epoch [{epoch+1}/{epochs}]\t Loss: {loss.item():.4f}\t Accuracy: {accuracy*100:.2f} %')
